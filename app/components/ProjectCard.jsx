43ff9b4062f89d3f0c503626c948e25faf6a19e7
guschi18 <philipp.guschl@gmx.de>
2024-03-24 01:31:50 +0100

    foto2.0

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
-const  ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
+const  ProjectCard = ({ imgUrl, title, description, previewUrl }) => {
   return (
     <div>
       <div className='rounded-t-xl relative group'>
       <img className='rounded-md relative group' 
       src={imgUrl} 
+      loading="eager"
       />
       <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] 
       bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
