c00b84a5c0e51b5ff21b3e31c7fea66630e88130
guschi18 <philipp.guschl@gmx.de>
2024-03-24 01:43:40 +0100

    foto.final.1

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -9,7 +9,7 @@ const projectsData = [
     id: 1,
     title: "Simple Wallet Connect",
     description: "Simple Wallet Connect von thirdweb bringt jede Website ins Web3",
-    image: '/images/projects/simple-wallet-connect.png',
+    image: '/images/projects/simple.png',
     tag: ["Alle", "Wallet"],
     previewUrl: "https://simple-wallet-connect-v2.vercel.app/",
   },
